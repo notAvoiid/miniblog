e2f7a4b4d0511680376e484381f855f380fdd848
Igor <igor.nacimento51@gmail.com>
2023-10-19 19:02:27 -0300

    "Styling the form and adjusting register form"

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,33 @@
+import styles from './Register.module.css';
+
+import { useState, useEffect } from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe as suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input type='text' name='displayName' required placeholder='Nome do usuario' ></input>
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input type='email' name='email' required placeholder='E-mail do usuario' ></input>
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input type='password' name='password' required placeholder='Insira sua senha' ></input>
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input type='password' name='confirmPassword' required placeholder='Confirme a sua senha' ></input>
+        </label>
+        <button className='btn'>Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
