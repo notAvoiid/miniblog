99922fcbbf026a7e09dcea83a7b1aa9b9ef47698
Igor <igor.nacimento51@gmail.com>
2023-10-19 19:20:05 -0300

    "Handling register submit"

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,28 +3,81 @@ import styles from './Register.module.css';
 import { useState, useEffect } from 'react';
 
 const Register = () => {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError('');
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+    
+    if(password !== confirmPassword) {
+      setError('As senhas precisam ser iguais!');
+      return;
+    }
+
+    console.log(user);
+  };
+
+
   return (
-    <div>
+    <div className={styles.register}>
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe as suas histórias</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Nome:</span>
-          <input type='text' name='displayName' required placeholder='Nome do usuario' ></input>
+          <input 
+            type='text' 
+            name='displayName' 
+            required 
+            placeholder='Nome do usuario' 
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
         </label>
         <label>
           <span>E-mail:</span>
-          <input type='email' name='email' required placeholder='E-mail do usuario' ></input>
+          <input 
+            type='email' 
+            name='email' 
+            required 
+            placeholder='E-mail do usuario'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           <span>Senha:</span>
-          <input type='password' name='password' required placeholder='Insira sua senha' ></input>
+          <input type='password' 
+            name='password' 
+            required 
+            placeholder='Insira sua senha'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
         </label>
         <label>
           <span>Confirmação de senha:</span>
-          <input type='password' name='confirmPassword' required placeholder='Confirme a sua senha' ></input>
+          <input type='password' 
+            name='confirmPassword' 
+            required 
+            placeholder='Confirme a sua senha' 
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </label>
         <button className='btn'>Cadastrar</button>
+        {error && <p className='error'>{error}</p>}
       </form>
     </div>
   );
