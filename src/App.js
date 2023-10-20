e900f2bec3c6922778f31c33e7ca9bb502b95936
Igor <igor.nacimento51@gmail.com>
2023-10-20 13:56:39 -0300

    "Links to connected users"

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import { AuthProvider } from './context/AuthContext';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/Createpost/Createpost'
+import Dashboard from './pages/Dashboard/Dashboard'
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -44,6 +48,8 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
+              <Route path='/posts/create' element={<CreatePost />} />
+              <Route path='/dashboard' element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
---
+++ src/pages/Createpost/Createpost.js
@@ -0,0 +1,13 @@
+import styles from './Createpost.module.css';
+
+const Createpost = () => {
+  return (
+    <div>
+      <h1>
+        Createpost
+      </h1>
+    </div>
+  )
+}
+
+export default Createpost;
