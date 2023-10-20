1ac585f81095922686b7c9fc343c873c9f8b8386
Igor <igor.nacimento51@gmail.com>
2023-10-20 17:23:30 -0300

    "Validating datas"

--- src/pages/Createpost/Createpost.js
+++ src/pages/Createpost/Createpost.js
@@ -16,19 +16,36 @@ const Createpost = () => {
 
   const { insertDocument, response } = useInsertDocument('posts'); 
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError('');
+
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError('A imagem precisa ser uma URL.');
+    }
+
+    const tagsArray = tags.split(',').map((tag) => tag.trim().toLowerCase());
+
+    if(!title || !image || !tags || !body) {
+      setFormError('Por favor, preencha todos os campos!');
+    };
+
+    if(formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid : user.uid,
       createdBy: user.displayName
     })
 
+    navigate('/')
   };
 
   return (
@@ -82,6 +99,7 @@ const Createpost = () => {
         {!response.loading && <button className='btn'>Cadastrar</button>}
         {response.loading && <button className='btn' disabled>Aguarde...</button>}
         {response.error && <p className='error'>{response.error}</p>}
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
