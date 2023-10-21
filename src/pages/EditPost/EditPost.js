781ff6e2df19137d763970a18ea0f24377361f4b
Igor <igor.nacimento51@gmail.com>
2023-10-20 22:12:32 -0300

    "Creating the option to update the posts"

--- src/pages/EditPost/EditPost.js
+++ src/pages/EditPost/EditPost.js
@@ -3,7 +3,7 @@ import styles from './EditPost.module.css';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
-import { useInsertDocument } from '../../hooks/useInsertDocument';
+import { useUpdateDocument } from '../../hooks/useUpdateDocument';
 import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 const Editpost = () => {
@@ -30,7 +30,7 @@ const Editpost = () => {
 
   const {user} = useAuthValue();
 
-  const { insertDocument, response } = useInsertDocument('posts'); 
+  const { updateDocument, response } = useUpdateDocument('posts'); 
 
   const navigate = useNavigate();
 
@@ -52,16 +52,18 @@ const Editpost = () => {
 
     if(formError) return;
 
-    insertDocument({
+    const data = {
       title,
       image,
       body,
       tagsArray,
       uid : user.uid,
       createdBy: user.displayName
-    })
+    };
 
-    navigate('/')
+    updateDocument(id, data);
+
+    navigate('/dashboard');
   };
 
   return (
